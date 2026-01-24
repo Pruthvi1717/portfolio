8bb9c3e0327f8f61b4cb227f200fa01914931926
Pruthvi1717 <pruthviraj.g@somaiya.edu>
2026-01-24 13:52:24 +0530

    added Profile image

--- src/Components/RightSide.jsx
+++ src/Components/RightSide.jsx
@@ -6,7 +6,7 @@ const RightSide = () => {
   return (
     <div className="right-visual">
       <div className="image-wrapper">
-        <img src="/ProfileI.jpg" alt="Developer Illustration" />
+        <img src="/ProfileImg.jpg" alt="Developer Illustration" />
       </div>
     </div>
   );
